179c13a09386449a6d58fb734d3d04ecb6709582
chaitenya-guvi <115387942+chaitenya-guvi@users.noreply.github.com>
2023-04-16 16:14:56 +0530

    JSON and assignment

---
+++ day11_assignment_json.py
@@ -0,0 +1,24 @@
+import json
+
+file_to_read = 'data/csvtojson_file.csv'
+
+with open(file_to_read, 'r') as file_obj:
+    variable1 = file_obj.read()
+
+variable1 = variable1.splitlines()
+
+list1 = []
+
+for itt in range(len(variable1)):
+    dict1 = dict(
+        club=variable1[itt].split(',')[0],
+        city=variable1[itt].split(',')[1],
+        country=variable1[itt].split(',')[2]
+    )
+
+    list1.append(dict1)
+
+file_to_write = 'data/json_file.txt'
+with open(file_to_write, 'w') as file_obj:
+    json.dump(list1, file_obj)
+
